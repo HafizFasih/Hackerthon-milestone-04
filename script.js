d83eb1b0f5ef5bfb0473f11cf9bc5330803a9aaf
Muhammad Fasih <161964251+HafizFasih@users.noreply.github.com>
2024-09-08 00:42:44 -0700

    4

---
+++ script.js
@@ -0,0 +1,141 @@
+var _a, _b;
+(_a = document.getElementById("resumeBtn")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", function () {
+    var resumeSection = document.querySelector(".resume");
+    var formSection = document.querySelector(".form");
+    if (resumeSection && formSection) {
+        resumeSection.style.display = "flex";
+        formSection.style.display = "none";
+    }
+});
+(_b = document.getElementById("customBtn")) === null || _b === void 0 ? void 0 : _b.addEventListener("click", function () {
+    var resumeSection = document.querySelector(".resume");
+    var formSection = document.querySelector(".form");
+    if (resumeSection && formSection) {
+        resumeSection.style.display = "none";
+        formSection.style.display = "flex";
+    }
+});
+function updateResumeContent(data) {
+    var setText = function (id, value) {
+        var element = document.getElementById(id);
+        if (element)
+            element.innerText = value;
+    };
+    setText('resumeName', data.name);
+    setText('resumeAddress', data.address);
+    setText('resumeEmail', data.email);
+    setText('resumeWebsite', data.website);
+    setText('resumeContact', data.contact);
+    var updateInnerHTML = function (id, html) {
+        var element = document.getElementById(id);
+        if (element)
+            element.innerHTML = html;
+    };
+    updateInnerHTML('resumeFacebook', "<span class=\"pic a\"></span> <a href=\"".concat(data.facebook, "\">Facebook</a>"));
+    updateInnerHTML('resumeTwitter', "<span class=\"pic b\"></span> <a href=\"".concat(data.twitter, "\">Twitter</a>"));
+    updateInnerHTML('resumeLinkedIn', "<span class=\"pic d\"></span> <a href=\"".concat(data.linkedin, "\">LinkedIn</a>"));
+    updateInnerHTML('resumeYouTube', "<span class=\"pic c\"></span> <a href=\"".concat(data.youtube, "\">YouTube</a>"));
+    updateInnerHTML('resumeAboutMe', data.about);
+    updateInnerHTML('resumeEducation', data.education);
+    updateInnerHTML('resumeWorkExperience', data.work);
+    var setStyleWidth = function (id, value) {
+        var element = document.getElementById(id);
+        if (element)
+            element.style.width = "".concat(value, "%");
+    };
+    setStyleWidth('resumeHtml', data.html);
+    setStyleWidth('resumeCss', data.css);
+    setStyleWidth('resumeJavascript', data.javascript);
+    setStyleWidth('resumeReact', data.react);
+    setStyleWidth('resumeNextjs', data.nextjs);
+    var customBtn = document.getElementById('customBtn');
+    if (customBtn)
+        customBtn.innerText = "EDIT";
+    var hobbies = document.getElementById("hobbies");
+    var developer = document.getElementById("developer");
+    if (hobbies)
+        hobbies.style.display = "none";
+    if (developer)
+        developer.style.display = "none";
+    if (data.profileImage) {
+        var profileImage = document.querySelector('.profile-image');
+        profileImage.style.backgroundImage = "url(".concat(data.profileImage, ")");
+    }
+}
+function handleFormSubmit(event) {
+    event.preventDefault();
+    var profileImageInput = document.getElementById('profileImage');
+    var profileImageUrl = '';
+    if (profileImageInput && profileImageInput.files && profileImageInput.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            var _a;
+            profileImageUrl = (_a = e.target) === null || _a === void 0 ? void 0 : _a.result;
+            var formData = {
+                name: document.getElementById('name').value,
+                address: document.getElementById('address').value,
+                email: document.getElementById('email').value,
+                website: document.getElementById('website').value,
+                contact: document.getElementById('contact').value,
+                facebook: document.getElementById('facebook').value,
+                twitter: document.getElementById('twitter').value,
+                linkedin: document.getElementById('linkedin').value,
+                youtube: document.getElementById('youtube').value,
+                about: document.getElementById('about').value,
+                education: document.getElementById('education').value,
+                work: document.getElementById('work').value,
+                html: parseInt(document.getElementById('html').value),
+                css: parseInt(document.getElementById('css').value),
+                javascript: parseInt(document.getElementById('javascript').value),
+                react: parseInt(document.getElementById('react').value),
+                nextjs: parseInt(document.getElementById('nextjs').value),
+                profileImage: profileImageUrl
+            };
+            updateResumeContent(formData);
+            var resumeSection = document.querySelector(".resume");
+            var formSection = document.querySelector(".form");
+            if (resumeSection && formSection) {
+                resumeSection.style.display = "flex";
+                formSection.style.display = "none";
+            }
+        };
+        reader.readAsDataURL(profileImageInput.files[0]);
+    }
+    else {
+        var formData = {
+            name: document.getElementById('name').value,
+            address: document.getElementById('address').value,
+            email: document.getElementById('email').value,
+            website: document.getElementById('website').value,
+            contact: document.getElementById('contact').value,
+            facebook: document.getElementById('facebook').value,
+            twitter: document.getElementById('twitter').value,
+            linkedin: document.getElementById('linkedin').value,
+            youtube: document.getElementById('youtube').value,
+            about: document.getElementById('about').value,
+            education: document.getElementById('education').value,
+            work: document.getElementById('work').value,
+            html: parseInt(document.getElementById('html').value),
+            css: parseInt(document.getElementById('css').value),
+            javascript: parseInt(document.getElementById('javascript').value),
+            react: parseInt(document.getElementById('react').value),
+            nextjs: parseInt(document.getElementById('nextjs').value)
+        };
+        updateResumeContent(formData);
+        var resumeSection = document.querySelector(".resume");
+        var formSection = document.querySelector(".form");
+        if (resumeSection && formSection) {
+            resumeSection.style.display = "flex";
+            formSection.style.display = "none";
+        }
+    }
+}
+document.addEventListener('DOMContentLoaded', function () {
+    var customForm = document.querySelector("form");
+    if (customForm) {
+        customForm.addEventListener('submit', handleFormSubmit);
+    }
+    else {
+        console.error('Form not found');
+    }
+});
---
+++ script.ts
@@ -0,0 +1,177 @@
+interface ResumeData {
+    name: string;
+    address: string;
+    email: string;
+    website: string;
+    contact: string;
+    facebook: string;
+    twitter: string;
+    linkedin: string;
+    youtube: string;
+    about: string;
+    education: string;
+    work: string;
+    html: number;
+    css: number;
+    javascript: number;
+    react: number;
+    nextjs: number;
+    profileImage?: string;
+}
+
+document.getElementById("resumeBtn")?.addEventListener("click", () => {
+    const resumeSection = document.querySelector(".resume") as HTMLElement;
+    const formSection = document.querySelector(".form") as HTMLElement;
+
+    if (resumeSection && formSection) {
+        resumeSection.style.display = "flex";
+        formSection.style.display = "none";
+    }
+});
+
+document.getElementById("customBtn")?.addEventListener("click", () => {
+    const resumeSection = document.querySelector(".resume") as HTMLElement;
+    const formSection = document.querySelector(".form") as HTMLElement;
+
+    if (resumeSection && formSection) {
+        resumeSection.style.display = "none";
+        formSection.style.display = "flex";
+    }
+});
+
+function updateResumeContent(data: ResumeData): void {
+    const setText = (id: string, value: string) => {
+        const element = document.getElementById(id) as HTMLElement;
+        if (element) element.innerText = value;
+    };
+
+    setText('resumeName', data.name);
+    setText('resumeAddress', data.address);
+    setText('resumeEmail', data.email);
+    setText('resumeWebsite', data.website);
+    setText('resumeContact', data.contact);
+
+    const updateInnerHTML = (id: string, html: string) => {
+        const element = document.getElementById(id) as HTMLElement;
+        if (element) element.innerHTML = html;
+    };
+
+    updateInnerHTML('resumeFacebook', `<span class="pic a"></span> <a href="${data.facebook}">Facebook</a>`);
+    updateInnerHTML('resumeTwitter', `<span class="pic b"></span> <a href="${data.twitter}">Twitter</a>`);
+    updateInnerHTML('resumeLinkedIn', `<span class="pic d"></span> <a href="${data.linkedin}">LinkedIn</a>`);
+    updateInnerHTML('resumeYouTube', `<span class="pic c"></span> <a href="${data.youtube}">YouTube</a>`);
+
+    updateInnerHTML('resumeAboutMe', data.about);
+    updateInnerHTML('resumeEducation', data.education);
+    updateInnerHTML('resumeWorkExperience', data.work);
+
+    const setStyleWidth = (id: string, value: number) => {
+        const element = document.getElementById(id) as HTMLElement;
+        if (element) element.style.width = `${value}%`;
+    };
+
+    setStyleWidth('resumeHtml', data.html);
+    setStyleWidth('resumeCss', data.css);
+    setStyleWidth('resumeJavascript', data.javascript);
+    setStyleWidth('resumeReact', data.react);
+    setStyleWidth('resumeNextjs', data.nextjs);
+
+    const customBtn = document.getElementById('customBtn') as HTMLElement;
+    if (customBtn) customBtn.innerText = "EDIT";
+
+    const hobbies = document.getElementById("hobbies") as HTMLElement;
+    const developer = document.getElementById("developer") as HTMLElement;
+
+    if (hobbies) hobbies.style.display = "none";
+    if (developer) developer.style.display = "none";
+
+    if (data.profileImage) {
+        const profileImage = document.querySelector('.profile-image') as HTMLElement;
+        profileImage.style.backgroundImage = `url(${data.profileImage})`;
+    }
+}
+
+function handleFormSubmit(event: Event): void {
+    event.preventDefault(); 
+
+    const profileImageInput = document.getElementById('profileImage') as HTMLInputElement;
+    let profileImageUrl = '';
+
+    if (profileImageInput && profileImageInput.files && profileImageInput.files[0]) {
+        const reader = new FileReader();
+        reader.onload = function (e: ProgressEvent<FileReader>) {
+            profileImageUrl = e.target?.result as string;
+
+            const formData: ResumeData = {
+                name: (document.getElementById('name') as HTMLInputElement).value,
+                address: (document.getElementById('address') as HTMLInputElement).value,
+                email: (document.getElementById('email') as HTMLInputElement).value,
+                website: (document.getElementById('website') as HTMLInputElement).value,
+                contact: (document.getElementById('contact') as HTMLInputElement).value,
+                facebook: (document.getElementById('facebook') as HTMLInputElement).value,
+                twitter: (document.getElementById('twitter') as HTMLInputElement).value,
+                linkedin: (document.getElementById('linkedin') as HTMLInputElement).value,
+                youtube: (document.getElementById('youtube') as HTMLInputElement).value,
+                about: (document.getElementById('about') as HTMLTextAreaElement).value,
+                education: (document.getElementById('education') as HTMLTextAreaElement).value,
+                work: (document.getElementById('work') as HTMLTextAreaElement).value,
+                html: parseInt((document.getElementById('html') as HTMLInputElement).value),
+                css: parseInt((document.getElementById('css') as HTMLInputElement).value),
+                javascript: parseInt((document.getElementById('javascript') as HTMLInputElement).value),
+                react: parseInt((document.getElementById('react') as HTMLInputElement).value),
+                nextjs: parseInt((document.getElementById('nextjs') as HTMLInputElement).value),
+                profileImage: profileImageUrl 
+            };
+
+            updateResumeContent(formData);
+
+            const resumeSection = document.querySelector(".resume") as HTMLElement;
+            const formSection = document.querySelector(".form") as HTMLElement;
+
+            if (resumeSection && formSection) {
+                resumeSection.style.display = "flex";
+                formSection.style.display = "none";
+            }
+        };
+        reader.readAsDataURL(profileImageInput.files[0]);
+    } else {
+        const formData: ResumeData = {
+            name: (document.getElementById('name') as HTMLInputElement).value,
+            address: (document.getElementById('address') as HTMLInputElement).value,
+            email: (document.getElementById('email') as HTMLInputElement).value,
+            website: (document.getElementById('website') as HTMLInputElement).value,
+            contact: (document.getElementById('contact') as HTMLInputElement).value,
+            facebook: (document.getElementById('facebook') as HTMLInputElement).value,
+            twitter: (document.getElementById('twitter') as HTMLInputElement).value,
+            linkedin: (document.getElementById('linkedin') as HTMLInputElement).value,
+            youtube: (document.getElementById('youtube') as HTMLInputElement).value,
+            about: (document.getElementById('about') as HTMLTextAreaElement).value,
+            education: (document.getElementById('education') as HTMLTextAreaElement).value,
+            work: (document.getElementById('work') as HTMLTextAreaElement).value,
+            html: parseInt((document.getElementById('html') as HTMLInputElement).value),
+            css: parseInt((document.getElementById('css') as HTMLInputElement).value),
+            javascript: parseInt((document.getElementById('javascript') as HTMLInputElement).value),
+            react: parseInt((document.getElementById('react') as HTMLInputElement).value),
+            nextjs: parseInt((document.getElementById('nextjs') as HTMLInputElement).value)
+        };
+
+        updateResumeContent(formData);
+
+        const resumeSection = document.querySelector(".resume") as HTMLElement;
+        const formSection = document.querySelector(".form") as HTMLElement;
+
+        if (resumeSection && formSection) {
+            resumeSection.style.display = "flex";
+            formSection.style.display = "none";
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const customForm = document.querySelector("form") as HTMLFormElement;
+    if (customForm) {
+        customForm.addEventListener('submit', handleFormSubmit);
+    } else {
+        console.error('Form not found');
+    }
+});
